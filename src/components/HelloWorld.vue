f061bb06da5c4118f2bf0e94f607cb91a9fdb37c
IciIcifur <iciicifur@yandex.ru>
2025-06-30 00:20:55 +0300

    New branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ defineProps<{ msg: string }>();
 
   <div class="flex w-fit flex-col gap-4">
     <UButton
-      v-for="i of [1, 2, 3, 4, 5, 7, 9, 11, 12, 12, 15]"
+      v-for="i of [1, 2, 3, 4, 5, 7, 9, 11, 12, 12, 15, 17]"
       :key="i"
       color="primary"
       >Nuxt UI</UButton
