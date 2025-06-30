32c5ff7a04b279519f9f337004c68aac8f9e978e
IciIcifur <iciicifur@yandex.ru>
2025-06-30 22:20:57 +0300

    Refactored a bit

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import { useUserStore } from '../stores/userStore.ts';
+import { computed, reactive, ref } from 'vue';
 
 const { user, isAuthenticated, logout } = useUserStore();
+const items = ref([{ label: 'Вход' }, { label: 'Регистрация' }]);
+const activeTab = ref('0');
+
+const signInFormValue = reactive({});
+const signUpFormValue = reactive({});
+
+function onSubmit() {
+  if (activeTab.value === '0') console.log(signInFormValue);
+  else console.log(signUpFormValue);
+}
+
+const actionText = computed(() =>
+  activeTab.value === '0' ? 'Войти' : 'Зарегистрироваться',
+);
 </script>
 
 <template>
   <div class="flex w-full items-center justify-center">
-    <UCard v-if="isAuthenticated" variant="soft" class="max-w-md">
+    <UCard v-if="isAuthenticated" class="max-w-md">
       <template #header>
         <h4 class="h-8 w-full">Вы уже вошли в систему!</h4>
       </template>
@@ -14,7 +29,7 @@ const { user, isAuthenticated, logout } = useUserStore();
       <p>
         Вы уже выполнили вход в систему под никнеймом
         <span class="text-indigo-500 dark:text-indigo-400">{{
-          user?.nickname
+          user.nickname
         }}</span
         >. Хотите выйти из аккаунта?
       </p>
@@ -28,10 +43,32 @@ const { user, isAuthenticated, logout } = useUserStore();
             variant="ghost"
             color="error"
           />
-          <UButton to="/" label="На главную" variant="link" size="lg" />
+          <UButton to="/" label="На главную" variant="link" />
         </div>
       </template>
     </UCard>
-    <div v-else>Вы не вошли в систему</div>
+    <UCard v-else class="w-full max-w-md">
+      <UTabs v-model="activeTab" :items="items" color="neutral" class="w-full">
+        <template #content="{ index }"
+          ><SignInForm v-if="index === '0'" /> <SignUpForm v-else />
+        </template>
+      </UTabs>
+
+      <template #footer>
+        <div class="flex h-8 items-center justify-between gap-4">
+          <UButton
+            to="/"
+            label="Вернуться на главную"
+            variant="link"
+            color="neutral"
+          />
+          <UButton
+            @click.prevent="onSubmit"
+            :label="actionText"
+            variant="solid"
+          />
+        </div>
+      </template>
+    </UCard>
   </div>
 </template>
